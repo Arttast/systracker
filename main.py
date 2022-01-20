c48c20273fb09f30ccf716ff08387c1f0e29faa8
Kuzey <kuzeyyanik2020@gmail.com>
2022-01-21 00:06:17 +0300

    Message Test 1

--- main.py
+++ main.py
@@ -4,19 +4,47 @@ import psutil
 import json
 import platform
 import datetime
+import distro
 from hurry.filesize import size,si
 from time import sleep
+if platform.system() == "Windows":
+    import win32serviceutil
+else:
+    winno = True
 class App:
-    def main(self):
+    def handle(self):
+        if winno != True:
+            for i in self.servres:
+                if self.servres[i] <2:
+                    self.status = "ST_MSG"
+                    self.msg = "Serv. Restart 3."
+        if self.status == "ST_MSG":
+            self.data = {"STATUS":"ST_MSG","MSG0":self.msg[0],"MSG1":self.msg[1]}
+            self.sendJSON()
+            self.status = "ST_NOM"
         if self.status == "ST_NOM":
-            self.data = {"STATUS":"ST_NOM","CPU":psutil.cpu_percent(0.5),"RP":psutil.virtual_memory().percent,"FD":size(psutil.disk_usage("/").free,system=si),"TD":size(psutil.disk_usage("/").total,system=si)}
+            if platform.system() == "Linux":
+                v = distro.name(True)
+            else:
+                v = platform.platform(False,True)
+            self.data = {"STATUS":"ST_NOM","CPU":psutil.cpu_percent(0.5),"RP":psutil.virtual_memory().percent,"FD":size(psutil.disk_usage("/").free,system=si),"TD":size(psutil.disk_usage("/").total,system=si),"CT":datetime.datetime.now().time(),"V":v,"py":platform.python_version()}
             self.sendJSON()
     status = "ST_NOM"
     useEmu = True
     data = {}
+    msg = ()
+    argv = {}
+    servres = {}
+    def connect(self):
+        if not winno:
+            for i in self.argv["ser"]:
+                if win32serviceutil.QueryServiceStatus(self.argv["ser"][i],None) != win32serviceutil.SERVICE_RUNNING:
+                    win32serviceutil.RestartService(self.argv["ser"][i])
+                    self.servres[self.argv["ser"][i]] = self.servres[self.argv["ser"][i]] + 1
+            
     def sendJSON(self):
         if self.useEmu:
-            DispEmu.parseJSON(self.data)
+            DispEmu.parseJSON(self.data,self.status)
             return 0
         temp = json.dumps(self.data)
         f = open("./test/output.txt","w+t") 
@@ -44,24 +72,31 @@ class DispEmc:
         print("|"+str(buf[0])+"|")
         print("|"+str(buf[1])+"|")
         print("+"+"----------------"+"+")
-    def parseJSON(self,json):
+    def parseJSON(self,json,st):
         buf = []
-        if self.state == 0:
-            buf.append("sd")
-            buf.append("asd")
-        if self.state == 1:
-            buf.append("CPU " + str(json["CPU"])+"%")
-            buf.append("RAM " + str(json["RP"])+"% used")
-        if self.state == 2:
-            buf.append("HD "+str(json["FD"])+"/"+str(json["TD"])+" Free")
-            buf.append("OS "+platform.platform(True,True))
-        self.println(buf)
-        if self.state == 2 :
-            self.state = 0
-        else:
-            self.state = self.state + 1
+        if st == "ST_NOM":
+            if self.state == 0:
+              buf.append(str(json["CT"]))
+              buf.append("Python v"+str(json["py"]))
+            if self.state == 1:
+                buf.append("CPU " + str(json["CPU"])+"%")
+                buf.append("RAM " + str(json["RP"])+"% used")
+            if self.state == 2:
+                buf.append("HD "+str(json["FD"])+"/"+str(json["TD"])+" Free")
+                buf.append("OS "+str(json["V"]))
+            self.println(buf)
+            if self.state == 2 :
+                self.state = 0
+            else:
+                self.state = self.state + 1
+        elif st == "ST_MSG":
+            buf.append(str(json["MSG0"]))
+            buf.append(str(json["MSG1"]))
+            self.println(buf)
+            input()
 DispEmu = DispEmc()
 ss = App()
+ss.connect()
 while True:
-    ss.main()
+    ss.handle()
     sleep(0.5)
